c0045016edf408acff52bae22494bb0dc4042fed
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-08-12 04:07:34 +0300

    Call library functions in jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildNode() {
-    echo "Building the application..."    
-    sh "npm install"
-}
-
-def buildImage() {
-    echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t neededcofe/blog-coffee:NJs-BC-2.0 . "
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push neededcofe/blog-coffee:NJs-BC-2.0"
-    }
-}
-
 def deployApp() {
     echo "Deploying the application..."
 }
